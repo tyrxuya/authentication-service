44f67251dbcd79764cf31f4206d3cb51acbdb0a7
Ivan Georgiev <ivan2003444@gmail.com>
2024-08-15 22:28:35 +0300

    Refactor package of BaseController

--- rest/src/main/java/com/tinqinacademy/authentication/rest/controllers/BaseController.java
+++ rest/src/main/java/com/tinqinacademy/authentication/rest/controllers/BaseController.java
@@ -1,11 +1,11 @@
-package com.tinqinacademy.authentication.rest;
+package com.tinqinacademy.authentication.rest.controllers;
 
 import com.tinqinacademy.authentication.api.base.OperationOutput;
 import com.tinqinacademy.authentication.api.errors.ErrorOutput;
 import io.vavr.control.Either;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.RestController;
 
 public abstract class BaseController {
     public ResponseEntity<?> getOutput(Either<ErrorOutput, ? extends OperationOutput> result, HttpStatus status) {
