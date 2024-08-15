397ea8fd84fd4a47152df8d8e9fbc3d850a996dc
Ivan Georgiev <ivan2003444@gmail.com>
2024-08-15 22:52:25 +0300

    Added new converter

--- core/src/main/java/com/tinqinacademy/authentication/core/configurators/WebConfig.java
+++ core/src/main/java/com/tinqinacademy/authentication/core/configurators/WebConfig.java
@@ -1,13 +1,18 @@
 package com.tinqinacademy.authentication.core.configurators;
 
+import com.tinqinacademy.authentication.core.converters.RegisterInputToUser;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final RegisterInputToUser registerInputToUser;
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
-
+        registry.addConverter(registerInputToUser);
     }
 }
