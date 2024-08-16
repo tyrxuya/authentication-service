04a55aa2b20ffc38e34a8b3c32e38be989af2931
Ivan Georgiev <ivan2003444@gmail.com>
2024-08-16 11:15:47 +0300

    Implemented login operation

--- api/src/main/java/com/tinqinacademy/authentication/api/operations/login/LoginOutput.java
+++ api/src/main/java/com/tinqinacademy/authentication/api/operations/login/LoginOutput.java
@@ -3,10 +3,12 @@ package com.tinqinacademy.authentication.api.operations.login;
 import com.tinqinacademy.authentication.api.base.OperationOutput;
 import lombok.*;
 
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
 @Builder
 public class LoginOutput implements OperationOutput {
+    private String token;
 }
