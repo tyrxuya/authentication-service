3b4034e7d55162c824de10a92d6fd45f637f4b61
Ivan Georgiev <ivan2003444@gmail.com>
2024-08-16 13:05:00 +0300

    Added endpoints

---
+++ api/src/main/java/com/tinqinacademy/authentication/api/AuthRestApiPaths.java
@@ -0,0 +1,15 @@
+package com.tinqinacademy.authentication.api;
+
+public final class AuthRestApiPaths {
+    private static final String AUTH = "/auth";
+
+    public static final String REGISTER = AUTH + "/register";
+    public static final String LOGIN = AUTH + "/login";
+    public static final String LOGOUT = AUTH + "/logout";
+    public static final String CONFIRM_REGISTRATION = AUTH + "/confirm-registration";
+    public static final String RECOVER_PASSWORD = AUTH + "/recover-password";
+    public static final String CHANGE_PASSWORD = AUTH + "/change-password";
+    public static final String PROMOTE = AUTH + "/promote";
+    public static final String DEMOTE = AUTH + "/demote";
+    public static final String VALIDATE = AUTH + "/validate";
+}
