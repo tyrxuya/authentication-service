103a91e7604fd4609f6445ad83a699eb07b50b56
Ivan Georgiev <105276674+tyrxuya@users.noreply.github.com>
2024-08-18 19:42:22 +0300

    Revert "[AS-008] Implement logout operation resolves #18 (#20) (#22)"
    This reverts commit 26ffddd3eef6d3b6c8f1d5ec1c85fbfca48dfe55.

--- core/src/main/java/com/tinqinacademy/authentication/core/configurators/WebConfig.java
+++ core/src/main/java/com/tinqinacademy/authentication/core/configurators/WebConfig.java
@@ -1,9 +1,6 @@
 package com.tinqinacademy.authentication.core.configurators;
 
-import com.tinqinacademy.authentication.core.converters.LogoutInputToBlacklistedToken;
 import com.tinqinacademy.authentication.core.converters.RegisterInputToUser;
-import com.tinqinacademy.authentication.core.converters.UserToConfirmationInput;
-import com.tinqinacademy.authentication.core.converters.UserToRecoveryInput;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
@@ -13,15 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
     private final RegisterInputToUser registerInputToUser;
-    private final UserToConfirmationInput userToConfirmationInput;
-    private final UserToRecoveryInput userToRecoveryInput;
-    private final LogoutInputToBlacklistedToken logoutInputToBlacklistedToken;
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(registerInputToUser);
-        registry.addConverter(userToConfirmationInput);
-        registry.addConverter(userToRecoveryInput);
-        registry.addConverter(logoutInputToBlacklistedToken);
     }
 }
