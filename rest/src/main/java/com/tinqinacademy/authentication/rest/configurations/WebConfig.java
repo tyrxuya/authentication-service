d3a843620b1dd6b376c263f9232e7b3f5faf7ff7
Ivan Georgiev <ivan2003444@gmail.com>
2024-08-23 11:55:50 +0300

    Added interceptor and paths to be intercepted in WebConfig

--- rest/src/main/java/com/tinqinacademy/authentication/rest/configurations/WebConfig.java
+++ rest/src/main/java/com/tinqinacademy/authentication/rest/configurations/WebConfig.java
@@ -1,21 +1,28 @@
-package com.tinqinacademy.authentication.core.configurators;
+package com.tinqinacademy.authentication.rest.configurations;
 
+import com.tinqinacademy.authentication.api.paths.AuthRestApiPaths;
 import com.tinqinacademy.authentication.core.converters.LogoutInputToBlacklistedToken;
 import com.tinqinacademy.authentication.core.converters.RegisterInputToUser;
 import com.tinqinacademy.authentication.core.converters.UserToConfirmationInput;
 import com.tinqinacademy.authentication.core.converters.UserToRecoveryInput;
+import com.tinqinacademy.authentication.rest.interceptor.AuthInterceptor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class WebConfig implements WebMvcConfigurer {
     private final RegisterInputToUser registerInputToUser;
     private final UserToConfirmationInput userToConfirmationInput;
     private final UserToRecoveryInput userToRecoveryInput;
     private final LogoutInputToBlacklistedToken logoutInputToBlacklistedToken;
+
+    private final AuthInterceptor authInterceptor;
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
@@ -23,5 +30,15 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addConverter(userToConfirmationInput);
         registry.addConverter(userToRecoveryInput);
         registry.addConverter(logoutInputToBlacklistedToken);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        log.info("adding interceptor");
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns(AuthRestApiPaths.DEMOTE)
+                .addPathPatterns(AuthRestApiPaths.PROMOTE)
+                .addPathPatterns(AuthRestApiPaths.LOGOUT)
+                .addPathPatterns(AuthRestApiPaths.CHANGE_PASSWORD);
     }
 }
