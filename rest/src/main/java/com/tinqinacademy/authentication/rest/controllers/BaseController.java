14fada6ab931356f594d05b09a9e91227bac25d1
Ivan Georgiev <ivan2003444@gmail.com>
2024-08-23 12:05:49 +0300

    Refactor method name in BaseController

--- rest/src/main/java/com/tinqinacademy/authentication/rest/controllers/BaseController.java
+++ rest/src/main/java/com/tinqinacademy/authentication/rest/controllers/BaseController.java
@@ -5,10 +5,9 @@ import com.tinqinacademy.authentication.api.errors.ErrorOutput;
 import io.vavr.control.Either;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
 
 public abstract class BaseController {
-    public ResponseEntity<?> getOutput(Either<ErrorOutput, ? extends OperationOutput> result, HttpStatus status) {
+    public ResponseEntity<?> handleOperationResult(Either<ErrorOutput, ? extends OperationOutput> result, HttpStatus status) {
         return result.fold(
                 errorOutput -> new ResponseEntity<>(errorOutput, errorOutput.getStatus()),
                 output -> new ResponseEntity<>(output, status)
