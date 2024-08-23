3914a60b246d7be87340114bd68f59c0a8dd60cf
Ivan Georgiev <105276674+tyrxuya@users.noreply.github.com>
2024-08-23 12:19:15 +0300

    [AS-011] Fix security configurations resolves #26 (#31)
    * Added security scheme to SecurityConfig and removed authentication filter
    
    * Added documentation and secured endpoints
    
    * Removed authentication filter
    
    * Added enable feign clients annotation
    
    * Removed templates (moved to email client)
    
    * Moved api paths to new package.
    BUGFIX: fixed paths

--- rest/src/main/java/com/tinqinacademy/authentication/rest/AuthenticationApplication.java
+++ rest/src/main/java/com/tinqinacademy/authentication/rest/AuthenticationApplication.java
@@ -3,6 +3,7 @@ package com.tinqinacademy.authentication.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaRepositories(basePackages = {"com.tinqinacademy.authentication.persistence.repositories"})
 @EntityScan(basePackages = {"com.tinqinacademy.authentication.persistence.entities"})
 @EnableScheduling
+@EnableFeignClients(basePackages = {"com.tinqinacademy.authentication"})
 public class AuthenticationApplication {
 
     public static void main(String[] args) {
