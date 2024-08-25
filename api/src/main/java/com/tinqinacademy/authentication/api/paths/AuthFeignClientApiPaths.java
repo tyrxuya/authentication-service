24c68b1e18dc1f1e3ac7ac7899b2a54205c8a4e9
Ivan Georgiev <ivan2003444@gmail.com>
2024-08-26 00:30:16 +0300

    Add new REST API paths and feign client paths

--- api/src/main/java/com/tinqinacademy/authentication/api/paths/AuthFeignClientApiPaths.java
+++ api/src/main/java/com/tinqinacademy/authentication/api/paths/AuthFeignClientApiPaths.java
@@ -6,4 +6,6 @@ public final class AuthFeignClientApiPaths {
     private static final String SPACE_SYMBOL = " ";
 
     public static final String VALIDATE = POST + SPACE_SYMBOL + AuthRestApiPaths.VALIDATE;
+    public static final String GET_USER_DETAILS = POST + SPACE_SYMBOL + AuthRestApiPaths.GET_USER_DETAILS;
+    public static final String GET_USER = POST + SPACE_SYMBOL + AuthRestApiPaths.GET_USER;
 }
